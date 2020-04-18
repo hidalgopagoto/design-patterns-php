848335a550e612e51a3b8e680a1301900fe3351d
Alan Pagoto <alan.pagoto@gmail.com>
2020-04-18 00:29:05 -0300

    Tests for quantities for composite design pattern

--- Composite/index.php
+++ Composite/index.php
@@ -11,7 +11,7 @@ $cart = new Cart();
 $product = new Product();
 $product->setQuantity(3);
 
-$cart->addItem($product);
+$cart->addItem($product); // Added 3 products to cart
 
 $box = new Box();
 $product2 = new Product();
@@ -21,10 +21,24 @@ $cart->addItem($box);
 
 $box2 = new Box();
 $box3 = new Box();
+$box4 = new Box();
+$box5 = new Box();
+
 $product3 = new Product();
-$product3->setQuantity(2);
+$product3->setQuantity(5);
+
 $box3->addItem($product3);
+
 $box2->addItem($box3);
+
+$box4->addItem($box2);
+
 $cart->addItem($box2);
+$cart->addItem($box3);
+$cart->addItem($box4);
+
+$box5->addItem($box4);
+
+$cart->addItem($box5);
 
 echo $cart->count();
